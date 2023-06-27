8f1e443d01820379294ffa97a491c7443d698570
kamleshkumarkohli <kamleshkumarkohli.cse2019@dscet.ac.in>
2023-06-27 12:22:27 +0530

    Jspider initial basic programs

---
+++ printStatement.java
@@ -0,0 +1,21 @@
+public class printStatement {
+    public static void main(String[] args) {
+        // System.out.println("oh devarea pls bless compnies to start hiring");
+        // System.out.println();
+        /* 
+        expected output //
+        1 
+             A 
+                3 
+
+                C
+                5
+        7    D    
+         * 
+        */
+        System.out.println("1");
+
+
+    }
+    
+}
---
+++ programTest.java
@@ -0,0 +1,11 @@
+// public class programTest{
+//     public static void main(String[] args) {
+//         System.out.println("welcome program");
+//     }
+// }
+
+class programTest1{
+public static void main(String args[]){
+System.out.println("Hello Java");
+}
+}
