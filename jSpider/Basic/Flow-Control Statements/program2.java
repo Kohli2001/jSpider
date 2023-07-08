d0a2f4af1ec1f2b0544a7a0f3f8fb5ade01af3a9
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-07-09 03:13:14 +0530

    Class session  programs

---
+++ jSpider/Basic/Flow-Control Statements/program2.java
@@ -0,0 +1,14 @@
+class program2{
+    public static void main(String[] args) {
+        System.out.println("program started");
+        int x = 4;
+        if(x>7){
+            System.out.println("the number is above 7 ");
+
+        }
+        else{
+            System.out.println("number is below 7");
+        }
+        System.out.println("program ended");
+    }
+}
---
+++ jSpider/Basic/Flow-Control Statements/program3.java
@@ -0,0 +1,16 @@
+class program3{
+    public static void main(String[] args) {
+        System.out.println("program started");
+        int x = 7 ;
+        if(x>7){
+            System.out.println("the number is above 7");
+        }
+        else if(x==7){
+            System.out.println("the number is equal to 7 ");
+        }
+        else{
+            System.out.println("the number is below 7");
+        }
+        System.out.println("program ended");
+    }
+}
