ce6e37b2380988a1e18f9254459698f52b743e2e
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-07-09 03:15:21 +0530

    last session of operators programs

---
+++ jSpider/Basic/Operators/Operators/Program1.java
@@ -0,0 +1,12 @@
+//operators 
+
+class program1{
+    public static void main(String[] args) {
+        System.out.println(10+5); //additions 
+        System.out.println(10-5); //substractions 
+        System.out.println(10*5); //multiplications 
+        System.out.println(10/5); //divisons 
+        System.out.println(10%5); //modulus
+    }
+    
+}
