ceeab7fca737a0275b6ffb9d2bf652d261024edc
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-07-20 22:35:14 +0530

    . java flies

---
+++ jSpider/Programming_Classes/Programs/arithOperators.java
@@ -0,0 +1,43 @@
+package com;
+
+public class arithOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//CONCETINATION
+		
+		
+		System.out.println("Hello" +20+23); //Hello2023
+		System.out.println(20+10+"Hello");  //30Hello
+		
+		System.out.println("Hello" +'A'+'B'); //HelloAB
+		
+		System.out.println('A'+'B'); // 131 using octa-decimal
+		
+		System.out.println(20+'A');  // 85
+		
+		System.out.println("================================"); 
+		
+		System.out.println(5/2); //2
+		
+		System.out.println(5/2.0); //2.5 
+		
+		System.out.println(3/2); //1 
+		
+		System.out.println(3/2.0);  //1.5
+		
+		System.out.println(0/2);  // 0 
+		
+	     System.out.println(5/0.0); // infinity
+	     
+	     System.out.println(5.0/0); //infinity
+		
+		System.out.println(5/0);  // ERROR--->>>> ArithmeticException
+		
+		
+		
+
+	}
+
+}
---
+++ jSpider/Programming_Classes/Programs/relationOperator.java
@@ -0,0 +1,25 @@
+package com;
+
+public class relationOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+//		System.out.println(20>10>5);  // because java run from l to right and 20>10 is give result as true
+									  //	now this true >5 is like boolean value with int value so the error occuring .
+		
+		
+		System.out.println(9%2==0);  // false condition 
+		System.out.println(8%2==0);  // true condition
+		
+		System.out.println('A'>=100); // flase because A in ascii vlaues as 65 ans 65 >=100 is false conditions
+		
+		
+		System.out.println('A'<=100); // same just conditions follow trues.
+		
+		
+	}
+
+}
