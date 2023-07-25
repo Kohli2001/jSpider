8d7f45ac761e7a22c7ffab46453c6924fc54ca08
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-07-26 00:40:18 +0530

    Sum of two number using fn

---
+++ jSpider/Basic/Function/Program2.java
@@ -0,0 +1,14 @@
+class Program2{
+
+
+    public static int div(int a, int b){
+
+        int res= a/b;
+
+        return res;
+    }
+    public static void main(String[] args) {
+        System.out.println(div(45, 5));
+        
+    }
+}
