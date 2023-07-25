17a20765a6ab68f5a700867c10ce2c80b955898d
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-07-26 00:44:39 +0530

    concate using String fn

---
+++ jSpider/Basic/Function/Program4.java
@@ -0,0 +1,13 @@
+class Program4{
+
+
+    public static String concet(String a , String b ){
+
+        String result= a +" "+ b;
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println(concet("Hii" ,"Kamlesh"));
+        
+    }
+}
