e1ea5d0572dc258c1eb15c63b7caf514982b4edf
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-07-26 00:38:50 +0530

    Function_Folder

---
+++ jSpider/Basic/Function/program1.java
@@ -0,0 +1,10 @@
+class program1{
+
+    public static void add(int a , int b ){
+        int sum = a+b;
+        System.out.println(sum);
+    }
+    public static void main(String[] args) {
+        add(3, 2);
+    }
+}
