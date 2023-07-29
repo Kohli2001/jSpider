27db7d944f174ce8860764df3b5272121533ea7e
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-07-29 14:01:20 +0530

    urary using square()

--- jSpider/Basic/Function/Program13.java
+++ jSpider/Basic/Function/Program13.java
@@ -11,8 +11,37 @@ class Program13{
     public static void main(String[] args) {
 
         int a = 5 , b=2;
-        int res = square(a)+square(b)+2*a*b;
-        System.out.println("Hence  (a+b)^2 = "+res);
+        int res = square(a)+square(b)+2*a*b; // its using both assigning and calculating the respectively using function parameter
+                                            //Hence function parameter can be use  directely in expression as well . 
+
+
+
+        int res2 = square(a)+square(b)-2*a*b;
+
+        System.out.println(" (a+b)^2 = "+res); 
+        System.out.println(" (a-b)^2 = "+res2); 
+
+        int res3 = square(res2);
+        System.out.println("printing square of the square()function ");
+
+        System.out.println(res3);
+
+ System.out.println("----------------------------");
+
+        int res4 = res3++;
+
+        System.out.println(res3++); //increamented value will be print
+        
+        res3++;
+
+        System.out.println(res4); //assigned values will be printed value will be print
+
+        System.out.println(res3++);
+
+
+
+
+
         
     }
 }
