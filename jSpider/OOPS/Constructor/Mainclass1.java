9aefb11b8a9f74435d04449ec1160b156b946105
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-08-09 23:24:02 +0530

    it took both in on source file

--- jSpider/OOPS/Constructor/Mainclass1.java
+++ jSpider/OOPS/Constructor/Mainclass1.java
@@ -1,10 +1,23 @@
+class Demo2{
+    int x ;
+   double y;
+
+   Demo2(){
+    System.out.println(" Running Demo1 constructor");
+     x=10;
+     y=3.5;
+
+   }
+}
+
+
 class Mainclass1{
     public static void main(String[] args) {
         System.out.println("Main method started");
 
 
 
-        Demo1 d1 = new Demo1();
+         Demo2 d1 = new Demo2();
 
         System.out.println("X value is " +d1.x);
         System.out.println("Y value is " +d1.y);
