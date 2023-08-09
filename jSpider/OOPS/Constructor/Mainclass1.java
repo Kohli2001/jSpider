15c7130859ed67c3bf94b70ad49378f25251cbfe
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-08-09 23:18:39 +0530

    Constructor()

---
+++ jSpider/OOPS/Constructor/Mainclass1.java
@@ -0,0 +1,14 @@
+class Mainclass1{
+    public static void main(String[] args) {
+        System.out.println("Main method started");
+
+
+
+        Demo1 d1 = new Demo1();
+
+        System.out.println("X value is " +d1.x);
+        System.out.println("Y value is " +d1.y);
+
+        System.out.println("Main method Ended");
+    }
+}
