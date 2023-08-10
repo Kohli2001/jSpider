613860787ace3fae6350492f33dcfe3241a489d0
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-08-10 18:44:37 +0530

    parametriased constructor()

---
+++ jSpider/OOPS/Constructor/MainClass2.java
@@ -0,0 +1,11 @@
+class MainClass2{
+    public static void main(String[] args) {
+
+
+        //parameterized constructor () 
+
+        Demo3 d1 = new Demo3(100, 200);
+        System.out.println("x value is " +d1.x);
+        System.out.println("x value is " +d1.y);
+    }
+}
