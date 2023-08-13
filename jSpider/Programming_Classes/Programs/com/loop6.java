7edad930e691977ce1681c6ce640e7b98b507d35
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-08-13 19:44:33 +0530

    checking prime or not

---
+++ jSpider/Programming_Classes/Programs/com/loop6.java
@@ -0,0 +1,36 @@
+package com;
+
+public class loop6 {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int n = 6; int count =0;
+		
+		for(int i=1; i<=n; i++) {
+			
+			if(n%i==0)  {
+				
+				count++;
+			}
+		
+		}
+		
+		if(count==2) {
+			
+			System.out.println("Prime number");
+			
+		}
+		
+		else {
+			System.out.println("Not a prime");
+		}
+		
+		
+		
+
+	}
+
+}
