160df5f4e6ed8a8d2907f7117b824787e0dd8a8a
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-08-13 17:48:49 +0530

    loop demo

---
+++ jSpider/Programming_Classes/Programs/com/loopDemo.java
@@ -0,0 +1,25 @@
+package com;
+import java.util.Scanner;
+
+
+
+public class loopDemo {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =  new Scanner(System.in);
+		
+		System.out.println("enter the vlaues");
+		int n = sc.nextInt();
+		
+		for(int i=0; i<=n; i++) {
+			System.out.println(i);
+		}
+		
+		
+
+	}
+
+}
