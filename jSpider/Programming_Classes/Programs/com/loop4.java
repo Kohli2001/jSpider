28b229dd06aaf4afe5ede52f13022427b8e25c50
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-08-13 19:42:43 +0530

    factors count

---
+++ jSpider/Programming_Classes/Programs/com/loop4.java
@@ -0,0 +1,20 @@
+package com;
+
+public class loop4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//factors of the given number
+		
+		int n = 9;
+		for(int i=1; i<=n; i++) {
+			if (n%i==0) {
+				
+				System.out.println(i);
+			}
+			
+		}
+	}
+
+}
