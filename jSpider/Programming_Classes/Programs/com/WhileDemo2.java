d945831d2b00a65385632eb0455e564244f0ca4b
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-08-16 21:55:46 +0530

    while loop demo 2

---
+++ jSpider/Programming_Classes/Programs/com/WhileDemo2.java
@@ -0,0 +1,20 @@
+package com;
+
+public class WhileDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int i =0, sum=0 , n=10;
+		
+		do {
+			
+			i++;
+			System.out.println(i);
+			
+			
+		} while (n>=i);
+	}
+
+}
