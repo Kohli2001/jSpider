d5e119b35a9051c3a99571ccb584a533070b6768
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-08-19 19:55:56 +0530

    logical programming paper test Q

---
+++ jSpider/Java_Execuation Test/CodeTest1/CodeTest1.java
@@ -0,0 +1,20 @@
+package com;
+
+public class CodeTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//Question No 1
+		
+		int n = 3 , fact = 1;
+		
+			for(int  i =1; i<=n; i++) {
+				fact*=i;
+			}
+			System.out.println(fact);
+
+	}
+
+}
