c215d91abe0b29254ec9cfb1d72133d382d9cc8f
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-08-20 15:31:50 +0530

    Dining_Food Page

---
+++ jSpider/DemoProjects/Zomato App/Dining_Food.java
@@ -0,0 +1,203 @@
+package DemoProjects;
+
+public class Dining_Food {
+
+}
+//Near by Restraunts classes/list 
+
+class UltimatePunjabi{
+	double price= 1560.00;
+	String location="BTM Layout,jp Nagar,5th Cross ,2nd Building";
+	String rating=" 3.3/5 ";
+	String status="Open";
+	
+	void details() {
+		
+		System.out.println("Price /2 person:" +price);
+		System.out.println("Location: " +location);
+		System.out.println("Rating: " +rating);
+		System.out.println("Currentely : "+status);
+	}
+	
+}
+class LittiTwist{
+	
+	double price= 2500.00;
+	String location="BTM Layout,jp Nagar,18th Cross, Front Building";
+	String rating=" 3.8/5 ";
+	String status="Open";
+	
+       void details() {
+		
+		System.out.println("Price /2 person:" +price);
+		System.out.println("Location: " +location);
+		System.out.println("Rating: " +rating);
+		System.out.println("Currentely : "+status);
+	}
+}
+class KhmerKitchen{
+	double price= 3500.00;
+	String location="Banarghatta road ,jp Nagar,3rd  Cross, luxury delux apartments ";
+	String rating=" 4.0/5 ";
+	String status="Open";
+	
+	
+	
+	 void details() {
+			
+			System.out.println("Price /2 person:" +price);
+			System.out.println("Location: " +location);
+			System.out.println("Rating: " +rating);
+			System.out.println("Currentely : "+status);
+		}
+	
+}
+class NarmalaHouse{
+	double price= 4500.00;
+	String location="Banarghatta road ,jp Nagar,13th Cross, Royal Heritage Building ";
+	String rating=" 4.3/5 ";
+	String status="Off";
+	
+	 void details() {
+			
+			System.out.println("Price /2 person:" +price);
+			System.out.println("Location: " +location);
+			System.out.println("Rating: " +rating);
+			System.out.println("Currentely : "+status);
+		}
+	
+}
+
+//famous Restaurants list or classes 
+
+class CasaPiclosa{
+	
+	double price= 6500.00;
+	String location="MG road ,9th Cross, Royal Heritage Building ";
+	String rating=" 4.3/5 ";
+	String status="Open Now";
+	
+	 void details() {
+			
+			System.out.println("Price per person:" +price);
+			System.out.println("Location: " +location);
+			System.out.println("Rating: " +rating);
+			System.out.println("Currentely : "+status);
+		}
+	
+}
+class EmpireRestaurant{
+	double price= 15000.00;
+	String location="MG road ,3rd Cross, Royal Classic Heritage ";
+	String rating=" 4.7/5 ";
+	String status="Open Now";
+	
+	 void details() {
+			
+			System.out.println("Price per person:" +price);
+			System.out.println("Location: " +location);
+			System.out.println("Rating: " +rating);
+			System.out.println("Currentely : "+status);
+		}
+	
+}
+class TaproomByshocklate{
+	
+	double price= 27000.00;
+	String location="Diagonal Road , Bangalore ,India";
+	String rating=" 4.7/5 ";
+	String status="Open Now";
+	
+	 void details() {
+			
+			System.out.println("Price per person:" +price);
+			System.out.println("Location: " +location);
+			System.out.println("Rating: " +rating);
+			System.out.println("Currentely : "+status);
+	
+	 }
+	 
+}
+
+class DelightRestaurant{
+	
+	double price= 27950.00;
+	String location="Pan Asian , 3rd Floor , 222 vittal , Bangalore";
+	String rating=" 4.7/5 ";
+	String status="closed";
+	
+	 void details() {
+			
+			System.out.println("Price per person:" +price);
+			System.out.println("Location: " +location);
+			System.out.println("Rating: " +rating);
+			System.out.println("Currentely : "+status);
+		}
+	
+	
+}
+//Top rated restrautants list 
+//LE ARABIA Restaurant\n2.AB's Restaurant\n3.Spice Terrace\n4.24@43 Restaurant
+
+class  LeArabia{
+
+	double price= 27950.00;
+	String location="no 15 , Bannerghatta Main Rd , Bangalore";
+	String rating=" 4.0/5 ";
+	String status="Open Now";
+	
+	 void details() {
+			
+			System.out.println("Price per person:" +price);
+			System.out.println("Location: " +location);
+			System.out.println("Rating: " +rating);
+			System.out.println("Currentely : "+status);
+		}
+	
+}
+class ABs{
+
+	double price= 39990.00;
+	String location="2nd Floor CPR Tower, 100 feet Ring Rd,  Bangalore";
+	String rating=" 4.3/5 ";
+	String status="Open now";
+	
+	 void details() {
+			
+			System.out.println("Price per person:" +price);
+			System.out.println("Location: " +location);
+			System.out.println("Rating: " +rating);
+			System.out.println("Currentely : "+status);
+		}
+}
+class SpiceTerrace{
+	
+	double price= 49990.00;
+	String location="North Indian, 24JW Marriott bengaluru 1, Vittal Mall";
+	String rating=" 4.6/5 ";
+	String status="Open now";
+	
+	 void details() {
+			
+			System.out.println("Price per person:" +price);
+			System.out.println("Location: " +location);
+			System.out.println("Rating: " +rating);
+			System.out.println("Currentely : "+status);
+		}
+}
+class Karavelli{
+	double price= 39560.00;
+	String location="Vivanta , 66 , Residency Rd ,bangeluru";
+	String rating=" 4.6/5 ";
+	String status="closed";
+	
+	 void details() {
+			
+			System.out.println("Price per person:" +price);
+			System.out.println("Location: " +location);
+			System.out.println("Rating: " +rating);
+			System.out.println("Currentely : "+status);
+		}
+	
+}
+//=======================================Codeby@3K=========================================================================
