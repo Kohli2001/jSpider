0c45ecd7425088afaa26811a6ea74bda7e2cdee9
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-08-22 18:41:30 +0530

    inheritance Demo

---
+++ jSpider/OOPS/WorkSpace/ProjectDemo/src/jspiders/p2/MainClass1.java
@@ -0,0 +1,16 @@
+package jspiders.p2;
+
+public class MainClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Run r1 = new Run();
+		System.out.println("m value is "+r1.m);
+//		r1.method1();
+		r1.method2();
+		System.out.println("n value is "+r1.n);
+
+	}
+
+}
---
+++ jSpider/OOPS/WorkSpace/ProjectDemo/src/jspiders/p2/Run.java
@@ -0,0 +1,15 @@
+package jspiders.p2;
+import jspiders.p1.Sample3;
+
+public class Run extends Sample3 {
+	int n=200;
+	void method2() {
+		
+		System.out.println("running Method2");
+		Run r1 = new Run();
+		r1.method1();
+		
+		
+	}
+	
+}
