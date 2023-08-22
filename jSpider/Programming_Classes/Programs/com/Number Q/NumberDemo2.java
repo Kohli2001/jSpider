c3b31c52dc0044db8ae4b0278fb99b26ca712231
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-08-22 18:31:20 +0530

    Number-Q3

---
+++ jSpider/Programming_Classes/Programs/com/Number Q/NumberDemo2.java
@@ -0,0 +1,16 @@
+package com;
+import java.util.Scanner;
+
+public class NumberDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any digit number");
+		int n = sc.nextInt();
+			System.out.println("Remove Last one digit of given number and print : " +n/10);
+
+		}
+		
+	}
+
