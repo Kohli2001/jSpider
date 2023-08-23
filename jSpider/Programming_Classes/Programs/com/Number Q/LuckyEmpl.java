aca2065d769453d696b4f3996f99344a0ea12bb1
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-08-23 17:36:34 +0530

    lucky studnts

---
+++ jSpider/Programming_Classes/Programs/com/Number Q/LuckyEmpl.java
@@ -0,0 +1,51 @@
+package com;
+
+import java.util.Scanner;
+
+public class LuckyEmpl {
+	
+	static String isPrime(int n) {
+		int c=0;
+		for(int i =1; i<=n; i++) {
+			if(n%i==0) {
+				c++;
+			}
+				
+		}
+		return c==2?"Lucky Employee":"UnLucky Employee";
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Employee Id");
+		int id = sc.nextInt();
+		int c=0 , r=0 ,sum=0, count=0, empid=id;
+			while(id!=0) {
+				r=id%10;
+				count++;
+				if (count<=4) {
+					sum+=r;	
+				}
+				id=id/10;
+			}	
+				if (count>=6) {
+					System.out.println(isPrime(sum));	
+				}
+				else {
+					System.out.println("Minimum Length of id should be 6 digit");
+				}
+				
+				
+			}
+			
+		}
+		
+		
+	
+
+//}
+
