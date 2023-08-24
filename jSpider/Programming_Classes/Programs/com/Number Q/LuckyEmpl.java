ae1c19048ed0b66c262ecf3c81b63e5887df5572
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-08-24 17:14:32 +0530

    first 4 lucky emp

--- jSpider/Programming_Classes/Programs/com/Number Q/LuckyEmpl.java
+++ jSpider/Programming_Classes/Programs/com/Number Q/LuckyEmpl.java
@@ -14,38 +14,36 @@ public class LuckyEmpl {
 		}
 		return c==2?"Lucky Employee":"UnLucky Employee";
 	}
+	static int reverseDigit(int  n) {
+		int r=0, rev=0;
+		while(n!=0) {
+			r=n%10;
+			rev=(rev*10)+r;
+			n=n/10;
+		}
+		return rev;
+	}
 	
-	
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Employee Id");
 		int id = sc.nextInt();
-		int c=0 , r=0 ,sum=0, count=0, empid=id;
-			while(id!=0) {
-				r=id%10;
-				count++;
-				if (count<=4) {
-					sum+=r;	
-				}
-				id=id/10;
-			}	
-				if (count>=6) {
-					System.out.println(isPrime(sum));	
-				}
-				else {
-					System.out.println("Minimum Length of id should be 6 digit");
-				}
-				
-				
-			}
-			
+		int  r=0 ,sum=0, count=0, empid=id;
+		
+		int reverseId=reverseDigit(id);
+		
+		for(int i=1; i<=4; i++) {
+			r = reverseId%10;
+			sum+=r;
+			reverseId=reverseId/10;
 		}
+		System.out.println(isPrime(sum));
 		
 		
-	
-
-//}
+	}
+		
+			
+	}
 
