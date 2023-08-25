6c3418395a69e302def0e816d696ca8789d6b46a
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-08-25 19:31:58 +0530

    MainClass of Insta App

---
+++ jSpider/OOPS/WorkSpace/ProjectDemo/src/jspiders/overRiding/InstaApp.java
@@ -0,0 +1,36 @@
+package overRiding;
+
+public class InstaApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("================Instagram App===================");
+		
+		Instagram2023 i1 = new Instagram2023();
+		System.out.println("=============Calling===========");
+		i1.call();
+		System.out.println("=============Filters===========");
+		i1.filter();
+		System.out.println("=============Messages===========");
+		i1.message();
+		System.out.println("=============Posts===========");
+		i1.post();
+		System.out.println("=============Networking===========");
+		i1.netorking();
+		System.out.println("=============Live===========");
+		i1.live();
+		System.out.println("=============Reels===========");
+		i1.reels();
+		System.out.println("=============Strickers===========");
+		i1.stickers();
+		System.out.println("=============Stories===========");
+		i1.Stories();
+		System.out.println("=============Shops===========");
+		i1.shop();
+		System.out.println("=============Threads===========");
+		i1.threads();
+
+	}
+
+}
