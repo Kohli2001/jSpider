4faabd032a052c1587f5a756db48fbd3daf1b2ec
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-08-30 22:00:06 +0530

    Add files via upload

---
+++ jSpider/OOPS/WorkSpace/ProjectDemo/src/jspiders/Inheritance/Test.java
@@ -0,0 +1,26 @@
+package inheritance;
+
+public class Test {
+	//private int x = 10;
+	
+	int x =10;
+	void sip() {
+		
+	}
+	
+	
+
+}
+class Test2  extends Test{
+	@Override
+	void sip() {
+		System.out.println("ghjkjhgf");
+	}
+
+
+	}
+	
+	int y=10;
+	
+}
+
