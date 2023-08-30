274aa8ab3e23403ba5565cda5b6c8d441b9d24d1
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-08-30 22:00:47 +0530

    Add files via upload

---
+++ jSpider/OOPS/WorkSpace/ProjectDemo/src/jspiders/Inheritance/SupperRunner.java
@@ -0,0 +1,49 @@
+package inheritance;
+
+public class SupperRunner {
+	
+
+	//static int x =10;
+	
+	void disp() {
+		System.out.println("hryyy");
+		
+		class test1{
+			
+			 static int x=20;
+		}
+		System.out.println();
+		
+	}
+
+	
+	
+	public static void main(String[] args) {
+		//System.out.println(x);
+		
+		String String = "Hello";
+		System.out.println(String);
+		
+		SupperRunner t2 = new SupperRunner();
+		//System.out.println(t2.x);
+		 t2.disp();
+		 
+		 
+//	
+//		class test{
+//			 int x=10;
+//		}
+//		
+//		
+//		
+//		test t1 = new test();
+//		System.out.println(t1.x);
+//				 
+		
+		
+//		//B b1 = new B();
+//		C c1 = new C(1);
+	}
+}
+
+
