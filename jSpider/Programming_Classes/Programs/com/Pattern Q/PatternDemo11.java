0e8c93a9301cf71e401d3bc06677a68ce36bbdce
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-09-01 18:55:39 +0530

    Add files via upload

---
+++ jSpider/Programming_Classes/Programs/com/Pattern Q/PatternDemo11.java
@@ -0,0 +1,21 @@
+package com;
+
+public class PatternDemo11 {
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
+				System.out.print(i+j +" ");
+			}
+			System.out.println();
+		}
+	}
+	
+
+}
+
+/*2 3 4 5 6 
+3 4 5 6 7 
+4 5 6 7 8 
+5 6 7 8 9 
+6 7 8 9 10 */
