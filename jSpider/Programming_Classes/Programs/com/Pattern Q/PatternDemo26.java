f62dfe5eaf3b1bbfc14a1fb40372197be7ef67d3
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-09-04 20:48:34 +0530

    Add files via upload

---
+++ jSpider/Programming_Classes/Programs/com/Pattern Q/PatternDemo26.java
@@ -0,0 +1,25 @@
+package com;
+
+public class PatternDemo26 {
+	public static void main(String[] args) {
+		int n=7; 
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
+				System.out.print(i+j>=n+1?"* ":" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
+
+/*
+
+     * 
+    * * 
+   * * * 
+  * * * * 
+ * * * * * 
+ 
+
+*/
