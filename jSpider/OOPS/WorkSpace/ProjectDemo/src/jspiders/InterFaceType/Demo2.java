62e467a50cca2e94908d5129240a60fa221434c4
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-09-05 23:05:12 +0530

    Add files via upload

---
+++ jSpider/OOPS/WorkSpace/ProjectDemo/src/jspiders/InterFaceType/Demo2.java
@@ -0,0 +1,19 @@
+package interfaceType;
+
+public interface Demo2 {
+	
+	void dis();
+	
+	void test();
+	
+}
+class Sample implements Demo2{
+	
+	public void disp() {
+		System.out.println("implented in class sample");
+	}
+	
+	public void test() {
+		System.out.println("implented in class sample");
+	}
+}
