06382c21b772003d9e5494678a4e29a357e9bef2
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-09-05 23:00:34 +0530

    Add files via upload

---
+++ jSpider/Programming_Classes/Programs/com/ArrayDemo Q/ArrayDemo3.java
@@ -0,0 +1,16 @@
+package com;
+
+public class ArrayDemo3 {
+	public static void main(String[] args) {
+		int [] arr = {10,20,30};
+		int max = arr[0];
+		
+		for(int i=0; i<arr.length; i++) {
+			 if(arr[i]>max) {
+				 max=arr[i]; 
+				 System.out.println(max);
+			 }
+		}
+	}
+
+}
