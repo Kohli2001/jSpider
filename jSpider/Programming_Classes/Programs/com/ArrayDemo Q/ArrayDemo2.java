110deec867047a8275244005a0d3e47d2b44d391
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-09-05 22:59:41 +0530

    Add files via upload

---
+++ jSpider/Programming_Classes/Programs/com/ArrayDemo Q/ArrayDemo2.java
@@ -0,0 +1,26 @@
+package com;
+
+import java.util.Scanner;
+
+public class ArrayDemo2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter size of the Array");
+		int size = sc.nextInt();
+		int [] arr = new int[size];
+		
+		for(int i=0 ; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int max  = arr[0];
+		for(int i=0 ; i<arr.length; i++) {
+			if(arr[i]>max) {
+			  max=arr[i];
+			  
+		   }
+			
+		}
+		System.out.println(max);
+	}
+
+}
