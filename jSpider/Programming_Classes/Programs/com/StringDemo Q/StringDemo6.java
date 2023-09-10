60b225f18ce649678ea4f6c4ab187cbe0a7a7685
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-09-10 12:37:15 +0530

    Add files via upload

---
+++ jSpider/Programming_Classes/Programs/com/StringDemo Q/StringDemo6.java
@@ -0,0 +1,13 @@
+package com;
+
+public class StringDemo6 {
+	public static void main(String[] args) {
+		//cpncate the two string at last
+		
+		String s1 = "Java is a ";
+		String s2 = "Programming Language";
+		
+		System.out.println(s1+s2);
+	}
+
+}
