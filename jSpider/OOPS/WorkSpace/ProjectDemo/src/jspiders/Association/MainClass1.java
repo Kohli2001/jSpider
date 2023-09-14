cfc58e7a9222f29f6ff10081dfd413edc7d6bf76
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-09-14 18:43:41 +0530

    Add files via upload

---
+++ jSpider/OOPS/WorkSpace/ProjectDemo/src/jspiders/Association/MainClass1.java
@@ -0,0 +1,15 @@
+package Association;
+
+public class MainClass1 {
+	public static void main(String[] args) {
+		
+		Sample1 s1 = new Sample1();
+		System.out.println(s1.y);
+		s1.test();
+		
+	}
+
+	
+	
+	
+}
