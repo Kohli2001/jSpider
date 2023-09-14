ecb84bcdcba506707f72c1d78d1a2f2c01cb1cd6
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-09-14 18:43:06 +0530

    sample1

---
+++ jSpider/OOPS/WorkSpace/ProjectDemo/src/jspiders/Association/Sample1.java
@@ -0,0 +1,16 @@
+package Association;
+
+public class Sample1 {
+	
+	int y =200;
+	
+	void test() {
+		
+		System.out.println("Running test methods ");
+		//creating local referenace variable and local object
+		Demo1 d1 = new Demo1();
+		System.out.println(d1.x);
+		d1.disp();
+	}
+
+}
