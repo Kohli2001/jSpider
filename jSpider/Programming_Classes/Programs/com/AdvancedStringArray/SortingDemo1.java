0db3fe1bc4740378480ed134181eb09d442244bc
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-09-21 21:40:36 +0530

    Advanced Array and String

---
+++ jSpider/Programming_Classes/Programs/com/AdvancedStringArray/SortingDemo1.java
@@ -0,0 +1,17 @@
+package advancedArrayString;
+
+import java.util.Scanner;
+
+public class SortingDemo1 {
+	
+	//print the array elemet using forech loop
+	
+	public static void main(String[] args) {
+			int [] arr= {10,20,30,40,50,60};
+			for(int element : arr) {
+				System.out.print(element +" ");
+			}
+	}
+	
+
+}
