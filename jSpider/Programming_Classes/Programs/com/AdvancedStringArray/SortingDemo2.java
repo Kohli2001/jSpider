5396838ad0f782b5e1071fd6279159bd946fccdc
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-09-21 21:41:45 +0530

    Add files via upload

---
+++ jSpider/Programming_Classes/Programs/com/AdvancedStringArray/SortingDemo2.java
@@ -0,0 +1,21 @@
+package advancedArrayString;
+
+import java.util.Scanner;
+
+public class SortingDemo2 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the size of array");
+		int size = sc.nextInt();
+		int [] arr  = new int[size];
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		for(int ele: arr) {
+			for(int ele2 : arr) {
+				System.out.println(ele +" "+ele2);
+			}
+		}
+	}
+
+}
