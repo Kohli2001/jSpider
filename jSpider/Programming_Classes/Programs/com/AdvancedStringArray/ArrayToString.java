c636f2040054dc797155e580378802f7e6e091b1
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-09-22 19:25:03 +0530

    ArrayToString

---
+++ jSpider/Programming_Classes/Programs/com/AdvancedStringArray/ArrayToString.java
@@ -0,0 +1,12 @@
+package advancedArrayString;
+
+public class ArrayToString {
+	public static void main(String[] args) {
+		char [] c = {'K','a','m','l','e','s','h'};
+		
+//		String res= new String(c);
+//		System.out.println(res);
+		System.out.println(new String(c));
+	}
+
+}
