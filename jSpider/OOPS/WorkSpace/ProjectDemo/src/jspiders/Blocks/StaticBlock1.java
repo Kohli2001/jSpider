a43a7236d87a30f5c99ef24596c292bd75c64000
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-09-22 19:27:55 +0530

    Blocks

---
+++ jSpider/OOPS/WorkSpace/ProjectDemo/src/jspiders/Blocks/StaticBlock1.java
@@ -0,0 +1,13 @@
+package blocks;
+
+public class StaticBlock1 {
+	
+	static {
+		
+		System.out.println("static block 1");
+	}
+	public static void main(String[] args) {
+		System.out.println("main method started");
+	}
+
+}
