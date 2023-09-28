f99b72be429d3a6387373a23c40ac6ea53ef01f3
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-09-28 23:23:06 +0530

    printing string length and array length

---
+++ jSpider/Programming_Classes/Programs/com/AdvancedStringArray/LengthOfArray.java
@@ -0,0 +1,24 @@
+package advancedArrayString;
+
+public class LengthOfArray {
+	
+	public static void main(String[] args) {
+		
+		String s= "hello";
+		int i=0;
+		
+		try {
+			
+			while(true) {
+				char c=s.charAt(i);
+				i++;
+				
+			}
+		}
+		catch (Exception e) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
