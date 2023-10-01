4bcc867583f3916be57d4b9cbb53656ce08fd31b
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-10-01 16:47:14 +0530

    2D - Array

---
+++ jSpider/Programming_Classes/Programs/com/TwoDArray/PrintingTwoDArray.java
@@ -0,0 +1,16 @@
+package twoDArray;
+
+
+public class PrintingTwoDArray {
+	
+	public static void main(String[] args) {
+		int[][] arr= {{1,2,3}, {3,7,9,2},{1,22,3}};
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr[i].length; j++) {
+				System.out.print(arr[i][j] +" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
