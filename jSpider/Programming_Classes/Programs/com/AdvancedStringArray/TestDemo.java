8afe5e40f8e9d40b556a9fdd029428f493f42df7
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-10-10 19:34:38 +0530

    Add files via upload

---
+++ jSpider/Programming_Classes/Programs/com/AdvancedStringArray/TestDemo.java
@@ -0,0 +1,19 @@
+package programmingTest;
+
+public class TestDemo {
+	
+//  static	void m1(int a) {
+//		System.out.println("int arg");
+//	}
+ static	void m1(double a1) {
+		System.out.println("double arg");
+	}
+// static	void m1(float a3) {
+//		System.out.println("float arg");
+//	}
+	public static void main(String[] args) {
+		
+		m1('A');
+	}
+
+}
