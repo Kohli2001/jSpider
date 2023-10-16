26c76d760da4ddebbc6fa1c4aac49544fd98262e
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-10-16 20:50:51 +0530

    Add files via upload

---
+++ jSpider/LearningLibraryConcepts/src/MainClass2.java
@@ -0,0 +1,18 @@
+package learningList;
+
+import java.util.ArrayList;
+
+public class MainClass2 {
+	
+	public static void main(String[] args) {
+		
+		ArrayList l1 = new ArrayList();
+		int n= l1.size();
+		boolean res = l1.isEmpty();
+		
+		System.out.println("Total elements "+n);
+		System.out.println("Is Arrays Emapty "+res);
+		
+	}
+
+}
