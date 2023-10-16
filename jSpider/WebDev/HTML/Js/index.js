367d0e8ac9d99bac40a347e769fb798f63d44824
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-10-16 19:25:33 +0530

    JavaScripts

---
+++ jSpider/WebDev/HTML/Js/index.js
@@ -0,0 +1,40 @@
+console.log("Hello world");
+
+// var a=10;
+// var a=100; //re-decalarion and re initialization is posssible
+// console.log(a); 
+
+
+// let age = 19;
+// age = 25;   // re initialization is possible
+// console.log(age);
+
+
+// const yob = 2001;
+// const pi = 3.14;
+// console.log(pi);
+
+// var a = 10
+// var b = 20;
+
+// // var sum = a+b;
+// console.log(a+b);
+
+
+//INTERVIEW QUESTIONS 
+
+//DataType in Javascripts : which assign during the runTime 
+let a = 108.8;  //number
+
+let b="String"; //String
+
+let c = false;  //boolean
+
+let d ; //undefined 
+
+console.log(b);
+
+//Asssignments : KFC online.kfc.con.in/menu
+
+
+
