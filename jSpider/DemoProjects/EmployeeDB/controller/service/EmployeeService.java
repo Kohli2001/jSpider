81cbb0f25f405dd86cda41d9e4146eadd2be0846
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-11-02 00:17:18 +0530

    Services

--- jSpider/DemoProjects/EmployeeDB/controller/service/EmployeeService.java
+++ jSpider/DemoProjects/EmployeeDB/controller/service/EmployeeService.java
@@ -5,6 +5,13 @@ public interface EmployeeService {
 	
 	void addEmp();
 	void getAllEmp();
+	void getEmpyByid();
+	void findEmpByRangSal();
+	void getEmpBySal();
+	void findEmpbyDesgn();
+	void findEmpByAge();
+	void updateEmpByEid();
+	void removeEmpByEid();
 	
 
 }
