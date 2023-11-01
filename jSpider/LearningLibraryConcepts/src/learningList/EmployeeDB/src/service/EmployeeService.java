3950b5039ab936685368aeef2a76b9630b08495a
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-11-02 00:20:35 +0530

    ArrayList based projects

---
+++ jSpider/LearningLibraryConcepts/src/learningList/EmployeeDB/src/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package service;
+
+public interface EmployeeService {
+	
+	
+	void addEmp();
+	void getAllEmp();
+	void getEmpyByid();
+	void findEmpByRangSal();
+	void getEmpBySal();
+	void findEmpbyDesgn();
+	void findEmpByAge();
+	void updateEmpByEid();
+	void removeEmpByEid();
+	
+
+}
