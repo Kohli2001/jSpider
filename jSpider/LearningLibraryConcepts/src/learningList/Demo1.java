7334efe54b28adfc53c7bd905684b11fa5fe16fe
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-11-05 16:28:39 +0530

    Add files via upload

---
+++ jSpider/LearningLibraryConcepts/src/learningList/Demo1.java
@@ -0,0 +1,35 @@
+package learningList;
+
+import java.util.ArrayList;
+
+public class Demo1 {
+	
+	static int x = 200;
+	static void name() {
+		System.out.println("hello ji ");
+	}
+	public static void main(String[] args) {
+//		ArrayList l = new ArrayList();
+//		l.add("kamlesh");
+//		l.add(123);
+//		l.add(null);
+//		l.add("kamlesh");
+//		System.out.println(l);		
+//		l.add("ma");
+//		System.out.println(l);
+//		try {
+//			l.remove(6);
+//		}
+//		catch(IndexOutOfBoundsException e) {
+//			System.out.println("Length must be in range" +e);
+//		}
+//		
+//		System.out.println(l);
+//		l.add(2, "students");
+//		System.out.println(l);
+//	}
+}
+class sample extends Demo1{
+	
+}
+
