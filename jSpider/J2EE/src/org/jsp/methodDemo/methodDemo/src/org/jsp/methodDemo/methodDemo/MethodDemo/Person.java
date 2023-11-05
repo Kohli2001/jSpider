cdc95c0519e4128b2b2a89a68a3af6011a530f78
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-11-05 17:01:07 +0530

    Person

---
+++ jSpider/J2EE/src/org/jsp/methodDemo/methodDemo/src/org/jsp/methodDemo/methodDemo/MethodDemo/Person.java
@@ -0,0 +1,55 @@
+package org.jsp.methodDemo;
+
+public class Person {
+	private int age;
+	private String name;
+	private String gender;
+	private long phone;
+	
+	public Person(int age, String name, String gender, long phone) {
+		this.age = age;
+		this.name = name;
+		this.gender = gender;
+		this.phone = phone;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	
+	
+	
+	
+	
+	
+		
+
+}
