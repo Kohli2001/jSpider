49ae4bdf3b070e3e46e536e0faf5fa3883c82f81
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-11-06 21:40:59 +0530

    App

---
+++ jSpider/LearningLibraryConcepts/src/learningExceptions/ExceptionProjects/Client/app/MainClass.java
@@ -0,0 +1,14 @@
+package app;
+
+import jspider.client.Registration;
+
+public class MainClass {
+	public static void main(String[] args) {
+		
+		Registration reg = new Registration();
+		reg.submit("Kamlesh", 345678876, "kamlesh654@gmail.com", "kamlesh@2023", 32);
+		
+	}
+
+}
+	
