74ae02c0d1b01bbb6ff88eca74ad36a356061feb
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-11-11 09:51:24 +0530

    Add files via upload

---
+++ jSpider/WebDev/HTML/DOM Manipulations/Task3.js
@@ -0,0 +1,23 @@
+
+
+let result = document.getElementById('result');
+
+let btn = increament=()=>{
+
+  result.textContent++;
+  result.style.color="Green";
+
+    
+}
+let btn1 =decreament=()=>{
+   result.textContent--;
+   result.style.color="Red"
+
+}
+let btn2  = resset=()=>{
+    result.textContent=0;
+    result.style.color="Blue"
+
+
+
+}
