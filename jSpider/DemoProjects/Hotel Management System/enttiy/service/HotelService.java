46908415e45d7fb4fdae1744f2c7b47d973293ba
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-11-14 22:38:46 +0530

    services

---
+++ jSpider/DemoProjects/Hotel Management System/enttiy/service/HotelService.java
@@ -0,0 +1,17 @@
+package service;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+import enttiy.Customer;
+
+public interface HotelService {
+	void roomInfo();
+	void availableRoom();
+	void allocateRoom();
+	void checkout();
+	void findAllCustomer();
+	void getCustomerById();
+
+
+}
