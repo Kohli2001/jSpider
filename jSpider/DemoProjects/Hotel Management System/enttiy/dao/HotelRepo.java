2660e6d17bd8b1a0b6ff336129ed5e97085f278e
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-11-14 22:41:37 +0530

    database

---
+++ jSpider/DemoProjects/Hotel Management System/enttiy/dao/HotelRepo.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import enttiy.Customer;
+
+public interface HotelRepo {
+	
+	ArrayList<Customer> a1 = new ArrayList<Customer>();
+	LinkedHashMap<Integer, Customer> cm = new LinkedHashMap<Integer, Customer>();
+	LinkedHashMap<Integer, Customer> sm = new LinkedHashMap<Integer, Customer>();
+	LinkedHashMap<Integer, Customer> dm = new LinkedHashMap<Integer, Customer>();
+}
