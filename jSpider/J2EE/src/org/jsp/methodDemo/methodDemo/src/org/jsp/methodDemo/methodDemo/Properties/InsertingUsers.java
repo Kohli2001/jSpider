248c72a02f2a941b2af8600ed7f141d45401de03
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-11-17 09:52:45 +0530

    Add files via upload

--- jSpider/J2EE/src/org/jsp/methodDemo/methodDemo/src/org/jsp/methodDemo/methodDemo/Properties/InsertingUsers.java
+++ jSpider/J2EE/src/org/jsp/methodDemo/methodDemo/src/org/jsp/methodDemo/methodDemo/Properties/InsertingUsers.java
@@ -1,7 +1,12 @@
 package org.jsp.properties;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
 import java.util.Scanner;
 
 
@@ -9,8 +14,11 @@ public class InsertingUsers {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your Id");
+		int id = sc.nextInt();
 		System.out.println("Enter Your name");
 		String name = sc.nextLine();
+		sc.nextLine();
 		System.out.println("Enter Phone number");
 		long phone =sc.nextLong();
 		System.out.println("Enter your email");
@@ -18,10 +26,53 @@ public class InsertingUsers {
 		System.out.println("Create Password");
 		String pass =  sc.next();
 		
-		int i=1;
+		Properties p = new Properties();
 		Connection con = null;
 		PreparedStatement pst=null;
-		String qry = "insert into user values(i++,?,?,?,?)";
+		FileInputStream fin=null;
+		String qry = "insert into user values(?,?,?,?,?)";
+		
+		try {
+			fin=new FileInputStream("C:\\Users\\hp\\Desktop\\jSpider\\J2EE\\LearningProperties\\JDBC.properties");
+			p.load(fin);
+			con=DriverManager.getConnection(p.getProperty("url"), p);
+			pst=con.prepareStatement(qry);
+			pst.setInt(1,id);
+			pst.setString(2, name);
+			pst.setLong(3, phone);
+			pst.setString(4, email);
+			pst.setString(5, pass);
+			int r =pst.executeUpdate();
+			System.out.println(r+ " rows Inserted");
+			
+		}
+		catch(IOException|SQLException  e) {
+			e.printStackTrace();
+		}
+		finally {
+				if(con!=null) {
+					try {
+						con.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				if(pst!=null) {
+					try {
+						pst.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				if(fin!=null) {
+					try {
+						fin.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			
+		}
 	}
 
 }
