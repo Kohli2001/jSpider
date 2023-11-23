a216cc2f07bd695b7bae19806be6a89b767f66a2
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-11-23 21:46:07 +0530

    Add files via upload

---
+++ jSpider/DSA Programming/Basics/Basics/bstprogrammingTest/Program6.java
@@ -0,0 +1,17 @@
+package org.dsa.basics.bstprogrammingTest;
+
+public class Program6 {
+	
+	public static void main(String[] args) {
+		String s1="Apple";
+		String s2="Banana";
+		
+		char[] c1 = s1.toCharArray();
+		char[] c2= s2.toCharArray();
+		
+		for(int i=0; i<s1.length(); i++) {
+			
+		}
+	}
+
+}
