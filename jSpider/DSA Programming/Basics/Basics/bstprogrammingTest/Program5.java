ca56d9188b4576ccf9da03a05767362ab28078d9
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-11-23 21:45:44 +0530

    Add files via upload

---
+++ jSpider/DSA Programming/Basics/Basics/bstprogrammingTest/Program5.java
@@ -0,0 +1,15 @@
+package org.dsa.basics.bstprogrammingTest;
+
+public class Program5 {
+	public static void main(String[] args) {
+		int num=8;
+		
+		for(int i=1; i<=num; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+
+}
