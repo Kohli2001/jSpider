48a9c665e6b43ca286363b8e7741ef9f8042de35
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-12-07 18:08:58 +0530

    Componenets Folder

---
+++ jSpider/WebDev/HTML/React_Js/firstapp/src/Components/Child.jsx
@@ -0,0 +1,13 @@
+
+
+const Child = (args) => {
+    let a = args;
+    console.log(a.bdy);
+    return ( 
+        <div className="child">
+            <h1>mere bachheeee...!</h1>
+        </div>
+     );
+}
+ 
+export default Child;
---
+++ jSpider/WebDev/HTML/React_Js/firstapp/src/Components/Component1.jsx
@@ -0,0 +1,15 @@
+import Component2 from "./Component2";
+const Component1 = () => {
+    return (  
+        <div className="componnet1">
+             <h1 style={{color:"blue"}}>i am  Components1</h1>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio excepturi magnam non officia vel velit, eum hic similique est at aliquid ullam laudantium in, modi, harum autem nobis dolor iste nihil? Impedit, blanditiis itaque?</p>
+             <hr />
+              <Component2/>;
+        </div>
+       
+
+    );
+}
+ 
+export default Component1;
