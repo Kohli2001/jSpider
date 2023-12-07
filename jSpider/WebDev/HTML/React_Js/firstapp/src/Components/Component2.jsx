3e0170039a1814655539eb43f2e2f70d78496070
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-12-07 18:09:24 +0530

    Add files via upload

---
+++ jSpider/WebDev/HTML/React_Js/firstapp/src/Components/Component2.jsx
@@ -0,0 +1,13 @@
+import Component3 from "./Component3";
+const Component2 = () => {
+    return ( 
+        <div className="component2">
+            <h1 style={{color:"blue"}}>I am Components2</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse accusamus adipisci ea praesentium perferendis quod illo ab obcaecati voluptatem excepturi? Cumque rem molestiae, corrupti numquam, quia molestias soluta accusamus blanditiis qui sint, modi libero harum eum! Aut expedita a facere.</p>
+            <hr />
+            <Component3/>
+        </div>
+     );
+}
+ 
+export default Component2;
