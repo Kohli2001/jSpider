27dcf1d29509cc15727bf4a3ec95d08d90c46b94
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-12-07 18:03:28 +0530

    Delete programTest2.java

--- programTest2.java
+++
@@ -1,6 +0,0 @@
-public class programTest2 {
-    public static void main(String[] args) {
-        // System.out.println(20+20+ "this is the number");
-        System.out.println("this is current year " +20+20);
-    }
-}
