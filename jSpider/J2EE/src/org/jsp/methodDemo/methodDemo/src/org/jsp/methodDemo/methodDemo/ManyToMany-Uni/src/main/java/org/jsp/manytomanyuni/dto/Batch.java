8131bc9efcc129be9a9c8597bce839d560203427
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-12-28 21:15:53 +0530

    Many to Many uni

---
+++ jSpider/J2EE/src/org/jsp/methodDemo/methodDemo/src/org/jsp/methodDemo/methodDemo/ManyToMany-Uni/src/main/java/org/jsp/manytomanyuni/dto/Batch.java
@@ -0,0 +1,80 @@
+package org.jsp.manytomanyuni.dto;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Batch {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(nullable = false)
+	private String subject;
+	@Column(nullable = false, unique = true)
+	private String batch_code;
+	@Column(nullable = false)
+	private String trainer;
+	
+	
+	
+	@ManyToMany(cascade = CascadeType.ALL)
+	private List<Student> students;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getBatch_code() {
+		return batch_code;
+	}
+
+	public void setBatch_code(String batch_code) {
+		this.batch_code = batch_code;
+	}
+
+	public String getTrainer() {
+		return trainer;
+	}
+
+	public void setTrainer(String trainer) {
+		this.trainer = trainer;
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+
+	@Override
+	public String toString() {
+		return "Batch [id=" + id + ", subject=" + subject + ", batch_code=" + batch_code + ", trainer=" + trainer
+				+ ", students=" + students + "]";
+	}
+	
+	
+	
+	
+	
+}
---
+++ jSpider/J2EE/src/org/jsp/methodDemo/methodDemo/src/org/jsp/methodDemo/methodDemo/ManyToMany-Uni/src/main/java/org/jsp/manytomanyuni/dto/Student.java
@@ -0,0 +1,82 @@
+package org.jsp.manytomanyuni.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false, unique = true)
+	private String email;
+	@Column(nullable = false, unique = true)
+	private long phone;
+	@Column(nullable = false)
+	private int yop;
+	@Column(nullable = false)
+	private double perc;
+	
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+
+	public int getYop() {
+		return yop;
+	}
+
+	public void setYop(int yop) {
+		this.yop = yop;
+	}
+
+	public double getPerc() {
+		return perc;
+	}
+	public void setPerc(double perc) {
+		this.perc = perc;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", yop=" + yop
+				+ ", perc=" + perc + "]";
+	}
+
+	
+
+}
